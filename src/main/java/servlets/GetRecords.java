f6c2e2756b490e4f763622d7f6539cfce1bc518b
Inigo <garomaru95@gmail.com>
2016-11-08 17:22:14 +0100

    Modify build.gradle

---
+++ src/main/java/servlets/GetRecords.java
@@ -0,0 +1,35 @@
+package servlets;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GetRecords extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor.
+     */
+    public GetRecords() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
--- src/main/java/servlets/temp.java
+++
@@ -1,7 +0,0 @@
-package servlets;
-
-/**
- * Created by ruben on 8/11/16.
- */
-public class temp {
-}
